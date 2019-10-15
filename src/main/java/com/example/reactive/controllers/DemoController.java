6f9e25bf99bc797c45822ef9b5cc97f60f0887a5
Kran Thaweesith <kran.tha@truedigital.com>
2019-10-15 09:20:23 +0700

    refactor to ReactiveElasticsearch

--- src/main/java/com/example/reactive/controllers/DemoController.java
+++ src/main/java/com/example/reactive/controllers/DemoController.java
@@ -1,13 +1,17 @@
 package com.example.reactive.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
-public class HomeController {
+public class DemoController {
+
     @GetMapping({"", "/"})
     public Mono<String> hello() {
+        log.debug("call hello method");
         return Mono.just("Hello world.");
     }
 }
